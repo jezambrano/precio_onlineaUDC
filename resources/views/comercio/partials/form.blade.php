4ef8f9a5cfcb0d15264a87dbfef7b6905e2462ce
Matias Bruzzo <mabruzzo@udc.edu.ar>
2016-10-17 14:30:57 -0300

    acomodo formulario comercio

--- resources/views/comercio/partials/form.blade.php
+++ resources/views/comercio/partials/form.blade.php
@@ -1,7 +1,26 @@
-
 <div class="form-group">
 	{!! Form::label('nombre','nombre del comercio (*) ',['class'=> "form-label-cms-3" ]) !!}
 	{!! Form::text('nombre',(isset($comercio)? $comercio->nombre: null),['class'=>'form-control','id' => 'nombre','required']) !!}
+</div>
+<div class="form-group">
+	{!! Form::label('rubro_id','Rubro al que pertenece el comercio(*)',['class=> "form-label"'])!!}
+	{!! Form::select('rubro', array();,'required'}
+</div>
+<div class="form-group">
+	{!! Form::label('provincia_id','Provincia al que pertenece el comercio(*)',['class=> "form-label"'])!!}
+	{!! Form::select('provincia', array();'required'}
+</div>
+<div class="form-group">
+	{!! Form::label('localidad_id','Localidad al que pertenece el comercio(*)',['class=> "form-label"'])}
+	{!! Form::select('localidad', array();'required'}
+</div>
+<div class="form-group">
+	{!! Form::label('direccion_calle','Direccion del Comercio (*) ',['class'=> "form-label-cms-3" ]) !!}
+	{!! Form::text('direccion_calle',(isset($comercio)? $comercio->direccion_calle: null),['class'=>'form-control','id' => 'direccion_calle','required']) !!}
+</div>
+<div class="form-group">
+	{!! Form::label('direccion_esquina','Esquina(*)',['class'=>"form-label-cms-3"])!!}
+	{!! Form::checkbox('direccion_esquina', '(isset($comercio)? $comercio->direccion_esquina: null),['class'=>'form-control','id' => 'direccion_esquina'', true);}
 </div>
 <div class="form-group">
 	{!! Form::label('telefeno','telefono del comercio (*) ',['class'=> "form-label-cms-3" ]) !!}
@@ -11,8 +30,5 @@
 {!! Form::label('horario','horario del comercio (*) ',['class'=> "form-label-cms-3" ]) !!}
 	{!! Form::text('horario',(isset($comercio)? $comercio->horario: null),['class'=>'form-control','id' => 'horario','required']) !!}
 </div>
-<div class="form-group">
-	{!! Form::label('direccion_calle','direccion (*) ',['class'=> "form-label-cms-3" ]) !!}
-	{!! Form::text('direccion_calle',(isset($comercio)? $comercio->direccion_calle: null),['class'=>'form-control','id' => 'direccion_calle','required']) !!}
-</div>
+
 	{!! Form::submit('Enviar',['class'=> "btn btn-default" ]) !!}
