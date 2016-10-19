7ba340cc5a31a16b908f578f3aa3544f4b5749ff
mccallizanavilca <mccallizanavilca@udc.edu.ar>
2016-10-19 13:34:17 -0300

    update vista

--- resources/views/comercio/index.blade.php
+++ resources/views/comercio/index.blade.php
@@ -1,39 +1,71 @@
-@extends('template.layout')
+@extends('template.master')
 
 @section('content')
 	
 	@if(isset($comercios))
         <div class="form-group">
-            <legend>Comercios</legend>
-            <div class="col-md-8">
-                <table class="table">
-                    <tr>
-                        <td>Nombre</td>
-                        <td>Rubro</td>
-                        <td>Localidad</td>
-                        <td>Horario</td>
-                        <td colspan="3">Acciones</td>
-                    </tr>
-                    @foreach ($comercios as $comercio)
-    	    		<tr>
-    	    			<td>{{ $comercio->nombre }} </td> 
-    		    		<td>{{ $comercio->rubro->nombre }} </td> 
-                        <td>{{ $comercio->localidad->nombre }} </td>
-    		    		<td>{{ $comercio->horario}}</td>
-                        <td><a href="{{route('comercio.edit',$comercio->id)}}">Editar</a></td>
-                        <td><a href="{{route('comercio.destroy',$comercio->id)}}">Borrar</a></td>
-                        <td><a href="{{route('comercio.show',$comercio->id)}}">Ver</a></td>
-			
-			<td style="text-align: center;"> 
-   {{ Form::open(array('url' => 'comercio/'.$comercio->id)) }}
-      {{ Form::hidden("_method", "DELETE") }}
-      {{ Form::submit("Eliminar") }}
-   {{ Form::close() }}
-</td>
-			
-			
-    	    		</tr>
-            		@endforeach
+            <h1>Listado de Comercios </h1>
+            <br>
+
+            <table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Rubro</th>
+            <th>Localidad</th>
+            <th>Horario</th>
+            <th colspan="3">acciones</th>
+
+        </tr>
+    </thead>
+
+    <tbody>
+  
+             @foreach ($comercios as $comercio)
+
+        <tr class="info">
+            <td>{{ $comercio->id }}</td>
+            <td>{{ $comercio->nombre }}</td>
+            <td>{{ $comercio->rubro->nombre }}</td>
+            <td>{{ $comercio->horario}}</td>
+            <td>
+                
+        {{ Form::open(array('url' => 'comercio/'.$comercio->id)) }}
+        {{ Form::hidden("_method", "show") }}
+        {{ Form::submit("ver") }}
+        {{ Form::close() }}
+
+
+            </td>
+            <td class="warning">
+                
+        {{ Form::open(array('url' => 'comercio/'.$comercio->id)) }}
+        {{ Form::hidden("_method", "show") }}
+        {{ Form::submit("Modificar") }}
+        {{ Form::close() }}
+
+
+
+
+            </td>
+            
+            <td class="danger">
+                
+        {{ Form::open(array('url' => 'comercio/'.$comercio->id)) }}
+        {{ Form::hidden("_method", "DELETE") }}
+        {{ Form::submit("Eliminar") }}
+        {{ Form::close() }}
+
+
+
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+
     	        </table>
             </div>
         </div>
