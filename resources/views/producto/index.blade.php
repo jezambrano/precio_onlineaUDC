96b42de28d52597f960bb76e377ec812ee9e1b83
Matias Bruzzo <mabruzzo@udc.edu.ar>
2016-10-21 20:18:07 -0300

    agrego inicio de producto

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -1,8 +1,76 @@
 @extends('template.layout')
-
+@section('title',"Producto Index")
 @section('content')
+    
+    <a href="{{ route('producto.create') }}" class="btn btn-success">Nuevo</a>
+    <br><br>
+
+    @if(isset($comercios))
+        <div class="form-group">
+            <h1>Listado de Productos </h1>
+            <br>
+
+            <table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+            <th>Precio</th>
+            <th>Codigo</th>
+            <th colspan="3">acciones</th>
+
+        </tr>
+    </thead>
+    <tfoot>
+    <tr>
+        <td colspan="2">
+        Total de Productos
+        </td>
+        <td>
+        {{ $producto-> total()}}
+        </td>
+        
+    </tr>
+    </tfoot>
+
+    <tbody>
+  
+             @foreach ($producto as $producto)
+
+        <tr>
+            <td>{{ $producto->id }}</td>
+            <td>{{ $producto->nombre }}</td>
+            <td>{{ $producto->descripcion }}</td>
+            <td>{{ $producto->precio }}</td>
+            <td>{{ $producto->codigo}}</td>
+            <td>
+                <a href='{{route('producto.show',$producto->id)}}' class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span></a>
+       
+
+            </td>
+            <td>
+        <a href='{{route('producto.edit',$producto->id)}}' class="btn btn-warning"><span class="glyphicon glyphicon-pencil"></span></a>
 
 
-    <h1>Producto cargado</h1>
-    <a class="btn btn-info" href="/"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Inicio</a>
+
+
+            </td>
+            
+            <td >
+        
+        {{ Form::model($producto, ["method" => "delete", "route" => ["producto.destroy", $producto->id], "class" =>"form-inline form-delete"]) }}
+        {{ Form::button("<span class='glyphicon glyphicon-trash'></span>", array("type" => "submit", "class" => "btn btn-danger delete")) }}
+        {{ Form::close()}}
+        
+
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+           {{ $producto->render() }}
+        </div>
+    @endif            
 @endsection
