a3926ef8678e219ba241eaa40a5696097f25807b
mccallizanavilca <mccallizanavilca@udc.edu.ar>
2016-10-22 00:15:44 -0300

    update

--- resources/views/comercio/formulario/create.blade.php
+++ resources/views/comercio/formulario/create.blade.php
@@ -1,1 +1,23 @@
 
+@extends('template.layout')
+@section('title',"Nuevo Comercio")
+@section('content')
+
+	
+    {!! Form::open(['url' => 'comercio', 'id'=>'comercio']) !!}
+
+            @include('comercio.partials.form')
+            
+    {!! Form::close() !!}
+
+
+
+@endsection
+
+@section('script')
+
+<script type="text/javascript">
+
+
+</script>
+@endsection
